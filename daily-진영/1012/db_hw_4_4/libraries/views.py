33ff339db2f9834d8c96c69beec53edd9e20ca8c
pangdoon <kimyoung100@naver.com>
2023-10-12 17:42:25 +0900

    1012

---
+++ daily-진영/1012/db_hw_4_4/libraries/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from .models import Book, Review
+
+# Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    context ={
+        'books':books,
+    } 
+    return render(request,'libraries/index.html',context)
+
+
+
+def detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    context ={
+        'book':book,
+    }
+    return render(request,'libraries/detail.html', context)
