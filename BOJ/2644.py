ba658067b6d5af0ea5d4a772f9f57471bf1238cf
pangdoon <kimyoung100@naver.com>
2023-11-01 19:46:10 +0900

    1101

--- BOJ/2644.py
+++ BOJ/2644.py
@@ -1,43 +1,33 @@
-import sys
-sys.setrecursionlimit(10**6)
-
-
 N = int(input())
-a, b = map(int, sys.stdin.readline().split())
-m = int(sys.stdin.readline())
+a, b = map(int, input().split())
+m = int(input())
 
 arr = [[] for _ in range(N+1)]
 
 for _ in range(m):
-    x, y = map(int, sys.stdin.readline().split())
+    x, y = map(int, input().split())
     arr[x].append(y)
     arr[y].append(x)
 
-result = 987654321
+result = -1  # 시작 시 result를 -1로 설정
 visited = [0] * (N+1)
-count = 0
-print(arr)
+count = 0  # count 변수를 함수 밖으로 뺍니다.
+
 def dfs(k):
     global result
     global count
     visited[k] = 1
+
     if k == b:
-        if result > count:
-            result = count
-            return
+        result = count
+        return
 
     for i in arr[k]:
         if visited[i] == 0:
-            visited[i] = 1
             count += 1
             dfs(i)
-            count -= 1
-
-
+            count -= 1  # 이전 노드로 돌아갈 때 count를 감소시킵니다.
 
 dfs(a)
-if result == 987654321:
-    print('-1')
-else:
-    print(result)
 
+print(result)
